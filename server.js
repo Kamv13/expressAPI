3153d90e74f0d9c7cf3c0ef0eb0b5ce748a91292
Kevin Mendoza <ksmv233@gmail.com>
2025-08-11 18:36:08 -0600

    cambiar privado a publico

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 8000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, Express!');
+app.get("/api/productos", (req, res) => {
+  const productos = [{ id: 1, nombre: "Laptop", precio: 1200 }];
+  res.json(productos);
 });
+
+app.get("/api/usuarios",(req, res) => {
+  const usuarios = [{
+    
+  }]
+})
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
